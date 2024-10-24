85a42dd5dd64c0821ee9fb7953698b84602f2b88
Alain Benbassat <alain@businessandcode.eu>
2024-10-24 14:36:00 +0200

    upgrade

--- sixlinesaddress.php
+++ sixlinesaddress.php
@@ -49,59 +49,12 @@ function sixlinesaddress_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function sixlinesaddress_civicrm_postInstall(): void {
-  _sixlinesaddress_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function sixlinesaddress_civicrm_uninstall(): void {
-  _sixlinesaddress_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function sixlinesaddress_civicrm_enable(): void {
   _sixlinesaddress_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function sixlinesaddress_civicrm_disable(): void {
-  _sixlinesaddress_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function sixlinesaddress_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _sixlinesaddress_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function sixlinesaddress_civicrm_entityTypes(&$entityTypes): void {
-  _sixlinesaddress_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
